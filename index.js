c4d4468176aea1d583102726bdc7c49f4eb4ac6a
Robert Howden <robert.howden@current-rms.com>
2020-04-22 08:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,16 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target')
 }
+
+function increaseRankBy(n) {
+  const lis = document
+  .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = (parseInt(lis[i].innerHTML) + 1).toString();
+}
+}
+
+function deepestChild() {
+  
+}
